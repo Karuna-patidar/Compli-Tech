6f63bb4719d2469128ba8dd63f8e1e6ef50aca15
karuna <karunapatidar02062gmail.com>
2025-09-13 18:44:19 +0530

    Initial commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { useState } from 'react';
-import logo from "../assets/logo.png";
 import { RiCloseLine, RiMenu2Line } from "@remixicon/react";
 const Navbar = () => {
     const [menu, openMenu] = useState(false);
     const [showMenu, setShowmenu] = useState(true);
   return (
-    <div className="flex flex-wrap justify-between md:items-center text-black p-2 bg-white">
-       <span className="text-3xl px-8 font-bold w-44"><img src={logo} alt="logo" /></span>
-       <ul className={`${ menu ? "block" : "hidden" } mx-10 p-y2 mt-4 font-semibold   p-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}>
+    <div className="flex flex-wrap justify-between md:items-center text-white p-2 bg-slate-900">
+       <span className="text-3xl px-8 font-bold">Protfolio</span>
+       <ul className={`${ menu ? "block" : "hidden" } mx-10 p-y2 mt-4 font-semibold  p-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`}>
        <a href="#home"><li  className="text-md transition-all duration-300 p-1 md:p-0">Home</li></a>
-       <a href ="#about"> <li  className="text-md transition-all duration-300 p-1 md:p-0">About Us</li> </a>
-       <a href ="#projects"> <li  className="text-md transition-all duration-300 p-1 md:p-0">Services</li> </a>
-       <a href="#contact"> <li  className="text-md transition-all duration-300 p-1 md:p-0">Contact Us</li> </a>
+       <a href ="#about"> <li  className="text-md transition-all duration-300 p-1 md:p-0">About</li> </a>
+       <a href ="#projects"> <li  className="text-md transition-all duration-300 p-1 md:p-0">Projects</li> </a>
+       <a href="#contact"> <li  className="text-md transition-all duration-300 p-1 md:p-0">Contact</li> </a>
        </ul>
        {showMenu ? (
         <RiMenu2Line
